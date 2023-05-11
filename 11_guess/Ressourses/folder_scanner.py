1b2ee16c4fc22982428961ac013350f44795b3d5
blacknevich <s-e-r-g-i-o@bk.ro>
2023-05-11 12:32:27 +0300

    flag is here

---
+++ 11_guess/Ressourses/folder_scanner.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+our_host = 'http://192.168.43.236/.hidden/'
+
+unique_texts = set()
+doc_number = 0
+
+def folder_scan(url):
+    global unique_texts, doc_number
+    req = requests.get(url)
+    text_from_site = BeautifulSoup(req.text, 'html.parser')
+    for a in text_from_site.find_all('a', href=True):
+        link = a['href']
+        if link[:2] == '..':
+            continue
+        elif link[-1] == '/':
+            folder_scan(url + link)
+        else:
+            doc_number += 1
+            print('\rscanning item #%d' %  (doc_number,), end='')
+            req2 = requests.get(url + link)
+            if req2.text not in unique_texts:
+                unique_texts.add(req2.text)
+                print('\n\t' + req2.text, url + link)
+
+if __name__ == '__main__':
+    folder_scan(our_host)
